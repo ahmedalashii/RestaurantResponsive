63c6f88fc3030eae8639f05f649ca4038fd1864f
ahmedalashii <62793355+ahmedalashii@users.noreply.github.com>
2020-12-09 21:43:20 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -70,3 +70,25 @@ document.getElementById("msg-email").onkeydown = function(){
 document.getElementById("message").onkeydown = function(){
     document.getElementById("msg").innerHTML = "";
 }
+/* Scroll To Top */
+
+var scrollToTopBtn = document.querySelector(".scrollToTopBtn")
+var rootElement = document.documentElement;
+
+function handleScroll() {
+  var scrollTotal = rootElement.scrollHeight - rootElement.clientHeight;
+  if ((rootElement.scrollTop / scrollTotal ) > 0) {
+    scrollToTopBtn.classList.add("showBtn")
+  } else {
+    scrollToTopBtn.classList.remove("showBtn")
+  }
+}
+
+function scrollToTop() {
+  rootElement.scrollTo({
+    top: 0,
+    behavior: "smooth"
+  })
+}
+scrollToTopBtn.addEventListener("click", scrollToTop)
+document.addEventListener("scroll", handleScroll)
